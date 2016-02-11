0e5b4dd4fca772b2dfa37e1a63549c5bda2bac67
Ty Enders <tybEnders@outlook.com>
2016-02-11 02:01:30 -0700

    Testing folder, end: 02112016

--- SortingClass.py
+++ SortingClass.py
@@ -38,6 +38,14 @@ class SortingUtilClass:
 		print('{}'.format(SortingUtilClass.list_to_string(print_list)).rjust(adjust))
 
 	@staticmethod
+	def print_comparison_simple(comparison):
+		fmt = 'Comparison #{}'.format(comparison)
+		if (comparison / 10) < 1:
+			print(fmt.rjust(18))
+		else:
+			print(fmt.rjust(19))
+
+	@staticmethod
 	def print_swap_level(array, swap, spacing, print_list, adjust):
 		print('Swap'.rjust(8), end=' ')
 		print('#{}'.format(swap).ljust(spacing), end='')
@@ -87,10 +95,11 @@ class SortingUtilClass:
 
 class SortingClass:
 
-	def __init__(self):
+	def __init__(self, toPrint=True):
 		self.comparisons = 1
 		self.swaps = 1
 		self.level = 1
+		self.print = toPrint
 
 	def set_defaults(self):
 		self.comparisons = 1
@@ -98,7 +107,8 @@ class SortingClass:
 		self.level = 1
 
 	def finish(self):
-		SortingUtilClass.print_results(self.comparisons, self.swaps)
+		if self.print:
+			SortingUtilClass.print_results(self.comparisons, self.swaps)
 		work = self.comparisons+(5*self.swaps)
 		return dict(comparisons=self.comparisons, swaps=self.swaps, work=work, level=self.level)
 
@@ -106,15 +116,18 @@ class SortingClass:
 		n = len(values)
 		for i in range(n):
 			# print loop level
-			SortingUtilClass.print_loop_position(i+1,values)
+			if self.print:
+				SortingUtilClass.print_loop_position(i+1,values)
 			for j in range(1,n):
 				# print comparison
-				SortingUtilClass.print_comparison_level(values, self.comparisons, 3*(j - 2) + 7, [values[j-1], values[j]], j)
+				if self.print:
+					SortingUtilClass.print_comparison_level(values, self.comparisons, 3*(j - 2) + 7, [values[j-1], values[j]], j)
 				self.comparisons += 1
 				if values[j-1] > values[j]:
 					values[j-1], values[j] = values[j], values[j-1]
 					# print swaps
-					SortingUtilClass.print_swap_level(values, self.swaps, 3*(j - 2) + 13, [values[j-1], values[j]], j)
+					if self.print:
+						SortingUtilClass.print_swap_level(values, self.swaps, 3*(j - 2) + 13, [values[j-1], values[j]], j)
 					self.swaps += 1
 		return self
 
@@ -122,31 +135,41 @@ class SortingClass:
 		n = len(values)
 		for i in range(1,n):
 			j = i
-			SortingUtilClass.print_loop_position(j, values)
+			if self.print:
+				SortingUtilClass.print_loop_position(j, values)
 			while j > 0 and values[j-1] > values[j]:
 				# print comparison
-				SortingUtilClass.print_comparison_level(values, self.comparisons, 3*(j - 1) + 4, [values[j-1], values[j]], j)
+				if self.print:
+					SortingUtilClass.print_comparison_level(values, self.comparisons, 3*(j - 1) + 4, [values[j-1], values[j]], j)
 				self.comparisons += 1
 				# swap values
 				values[j-1], values[j] = values[j], values[j-1]
 				# print swaps
-				SortingUtilClass.print_swap_level(values, self.swaps, 3*(j - 1) + 10, [values[j-1], values[j]], j)
+				if self.print:
+					SortingUtilClass.print_swap_level(values, self.swaps, 3*(j - 1) + 10, [values[j-1], values[j]], j)
 				self.swaps += 1
 				j -= 1
 			else:
 				# print comparison
-				SortingUtilClass.print_comparison_level(values, self.comparisons, 3*(j - 1) + 4, [values[j-1], values[j]], j)
+				if self.print:
+					SortingUtilClass.print_comparison_level(values, self.comparisons, 3*(j - 1) + 4, [values[j-1], values[j]], j)
 				self.comparisons += 1
 		return self
 
 	def selection_sort(self, values):
 		n = len(values)
 		count = 1
+
 		for i in range(n-1, 0, -1):
 			# print loop level
+			if self.print:
+				SortingUtilClass.print_loop_position(count, values)
 			maximum = self.max_key(0, i, values)
 			# swap values
 			values[maximum], values[i] = values[i], values[maximum]
+			# print swaps
+			if self.print:
+				SortingUtilClass.print_swap_level(values, self.swaps, 3*(i-1) + 10, [values[maximum], values[i]], i)
 			self.swaps += 1
 			count += 1
 		return self
@@ -155,10 +178,14 @@ class SortingClass:
 		largest = low
 		for j in range(low+1, high+1):
 			# print comparison
+			if self.print:
+				SortingUtilClass.print_comparison_level(values, self.comparisons, 3*(j - 1) + 4, [values[j-1], values[j]], j)
 			self.comparisons += 1
 			if values[largest] < values[j]:
 				largest = j
 			# print max and array
+			if self.print:
+				pass
 		return largest
 
 	# Assignment 4 methods
@@ -167,9 +194,11 @@ class SortingClass:
 		i = low
 
 		# print pivot
+		SortingUtilClass.print_pivot_level(pivot)
 		for j in range(low, high):
 			# print comparison
-
+			if self.print:
+				SortingUtilClass.print_comparison_simple(self.comparisons)
 			self.comparisons += 1
 			if values[j] <= pivot:
 				values[i], values[j] = values[j], values[i]
@@ -186,10 +215,15 @@ class SortingClass:
 		# print array
 		# print low
 		# print high
+		if self.print:
+			SortingUtilClass.print_level_with_array(self.level, values)
+			SortingUtilClass.print_high_low(high, low)
 		if low < high:
 			p = self.partition(values, low, high)
 			self.level += 1
 			# print 'Calling QS'...
+			if self.print:
+				SortingUtilClass.print_qs_fn(low, high, p)
 			self.quick_sort(values, low, p - 1)
 			self.quick_sort(values, p + 1, high)
 		return self
@@ -197,20 +231,24 @@ class SortingClass:
 	def all(self, orig):
 		values = copy(orig)
 		self.set_defaults()
-		SortingUtilClass.print_algorithm_title('Bubble Sort')
+		if self.print:
+			SortingUtilClass.print_algorithm_title('Bubble Sort')
 		self.bubble_sort(values).finish()
 
 		values = copy(orig)
 		self.set_defaults()
-		SortingUtilClass.print_algorithm_title('Insertion Sort')
+		if self.print:
+			SortingUtilClass.print_algorithm_title('Insertion Sort')
 		self.insertion_sort(values).finish()
 
 		values = copy(orig)
 		self.set_defaults()
-		SortingUtilClass.print_algorithm_title('Selection Sort')
+		if self.print:
+			SortingUtilClass.print_algorithm_title('Selection Sort')
 		self.selection_sort(values).finish()
 
 		values = copy(orig)
 		self.set_defaults()
-		SortingUtilClass.print_algorithm_title('Quick Sort')
+		if self.print:
+			SortingUtilClass.print_algorithm_title('Quick Sort')
 		self.quick_sort(values, 0, len(values) - 1).finish()
---
+++ test/SortingClassTest.py
@@ -0,0 +1,16 @@
+import unittest
+import SortingClass
+
+
+class SortingClassTest(unittest.TestCase):
+	def setUp(self):
+		self.sorter = SortingClass.SortingClass(False)
+
+	def test_init_settings(self):
+		self.assertEquals(self.sorter.comparisons, 1, 'set comparisons to 1')
+		self.assertEquals(self.sorter.swaps, 1, 'set swaps to 1')
+		self.assertEquals(self.sorter.level, 1, 'set level to 1')
+		self.assertEquals(self.sorter.print, False, 'do not print for testing')
+
+if __name__ == '__main__':
+	unittest.main()
